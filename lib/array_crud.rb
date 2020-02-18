8abdc8470205c8276e8bc3dd3172bba590d43366
premti <premti@gmail.com>
2020-02-18 17:30:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,9 +21,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  new_array = ["#{array}"]
-  new_array.pop()
-  return new_array
+  return array.pop()
 end
 
 def remove_element_from_start_of_array(array)
