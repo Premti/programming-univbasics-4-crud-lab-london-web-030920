9280c58498865bac836e7c3d6b8e4b3640280f7d
premti <premti@gmail.com>
2020-02-18 17:16:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = add_element_to_end_of_array.push
+  new_array = add_element_to_end_of_array.push(element)
   return new_array
 end
 
